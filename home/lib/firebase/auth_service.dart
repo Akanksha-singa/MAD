47d1ed10f89c54d6e9588b7e0848e46738e7aa39
Aneesh <aneeshkp1515@gmail.com>
2024-07-12 11:25:55 +0530

    Changes

--- home/lib/firebase/auth_service.dart
+++ home/lib/firebase/auth_service.dart
@@ -1,17 +1,16 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  // Sign in with Google
   Future<User?> signInWithGoogle() async {
     try {
-      final GoogleSignIn googleSignIn = GoogleSignIn();
-      final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
       if (googleUser == null) {
-        return null; // The user canceled the sign-in
+        print('Google Sign-In cancelled by user');
+        return null;
       }
 
       final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
@@ -23,57 +22,11 @@ class AuthService {
       final UserCredential userCredential = await _auth.signInWithCredential(credential);
       return userCredential.user;
     } catch (e) {
-      print(e.toString());
-      return null;
+      print('Error in signInWithGoogle: $e');
+      if (e is GoogleSignInException) {
+        print('Google Sign-In Exception: ${e.code}');
+      }
+      rethrow;
     }
   }
-
-// Sign in with Facebook
-//   Future<User?> signInWithFacebook() async {
-//     try {
-//       final _instance = FacebookAuth.instance;
-//       final result = await _instance.login(permissions: ['email']);
-//       if (result.status == LoginStatus.success) {
-//         final OAuthCredential credential = FacebookAuthProvider.credential(result.accessToken!.token);
-//         final UserCredential userCredential = await _auth.signInWithCredential(credential);
-//
-//         // Optionally update the user's email
-//         await _instance.getUserData().then((userData) async {
-//           await userCredential.user!.updateEmail(userData['email']);
-//         });
-//
-//         return userCredential.user;
-//       } else if (result.status == LoginStatus.cancelled) {
-//         throw Exception('Login cancelled');
-//       } else {
-//         throw Exception('Error logging in with Facebook');
-//       }
-//     } catch (e) {
-//       throw Exception(e.toString());
-//     }
-//   }
-
-
 }
-
-// Sign in with Apple
-// Future<User?> signInWithApple() async {
-//   try {
-//     final AuthorizationCredentialAppleID appleCredential = await SignInWithApple.getAppleIDCredential(
-//       scopes: [
-//         AppleIDAuthorizationScopes.email,
-//         AppleIDAuthorizationScopes.fullName,
-//       ],
-//     );
-//     final AuthCredential credential = OAuthProvider("apple.com").credential(
-//       idToken: appleCredential.identityToken,
-//       accessToken: appleCredential.authorizationCode,
-//     );
-//     final UserCredential userCredential = await _auth.signInWithCredential(credential);
-//     return userCredential.user;
-//   } catch (e) {
-//     print(e);
-//     return null;
-//   }
-// }
-
--- home/lib/login_screen.dart
+++ home/lib/login_screen.dart
@@ -1,235 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-// import 'package:flutter/services.dart';
-import 'settings_screen.dart';
-import 'create_account_four_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'password_screen.dart';
-import 'package:home/firebase/auth_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'auth_service.dart';
+import 'home_screen.dart'; // Import the HomeScreen widget
 
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  final TextEditingController phoneNumberController = TextEditingController();
-  final AuthService authService = AuthService();
+class LoginScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
-      ),
-      home: LoginScreen(),
-    );
-  }
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class LoginScreen extends StatelessWidget {
-  LoginScreen({Key? key}) : super(key: key);
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final AuthService _authService = AuthService();
 
-  final TextEditingController phoneNumberController = TextEditingController();
-  final AuthService authService = AuthService();
+  void _login() async {
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    try {
+      final user = await _authService.signIn(email, password);
+      print('Logged in successfully: $user');
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    } catch (e) {
+      print('Failed to log in: $e');
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Failed to log in: $e'),
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => SettingsScreen()),
-            );
-          },
-        ),
-        title: Text(
-          " Back",
-          style: TextStyle(color: Colors.white, fontSize: 16),
-        ),
+        title: Text('Login'),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(
-              child: Text(
-                "PWM",
-                style: TextStyle(
-                  color: Color(0xFFFF4D4D),
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
             ),
-            SizedBox(height: 40),
-            Center(
-              child: SvgPicture.asset(
-                  "assets/images/img_group_10.svg", height: 200),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
             ),
-            SizedBox(height: 40),
-            Text(
-              "Enter your",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              "mobile number",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 16),
-            _buildPhoneNumberInput(),
-            SizedBox(height: 16),
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => CreateAccountFourScreen()),
-                );
-              },
-              child: Text(
-                "Create Account",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            SizedBox(height: 16),
+            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => PasswordScreen()),
-                );
-              },
-              child: Text("Continue"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFFFF4D4D),
-                minimumSize: Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                textStyle: TextStyle(color: Colors.white),
-              ),
-            ),
-            SizedBox(height: 16),
-            Row(
-              children: [
-                Expanded(child: Divider(color: Colors.white)),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: Text("or continue using",
-                      style: TextStyle(color: Colors.white)),
-                ),
-                Expanded(child: Divider(color: Colors.white)),
-              ],
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // _socialButton(
-                //   "assets/images/img_facebook_f_logo_2019.svg",
-                //       () async {
-                //     // Implement Facebook sign-in logic here
-                //     User? user = await authService.signInWithFacebook();
-                //     if (user != null) {
-                //       print('Facebook Login successful: ${user.email}');
-                //     } else {
-                //       print('Facebook Login failed');
-                //     }
-                //   },
-                // ),
-                _socialButton(
-                  "assets/images/img_google.svg",
-                      () async {
-                    User? user = await authService.signInWithGoogle();
-                    if (user != null) {
-                      print('Google Login successful: ${user.email}');
-                    } else {
-                      print('Google Login failed');
-                    }
-                  },
-                ),
-                // _socialButton(
-                //   "assets/images/img_path1504.svg",
-                //       () async {
-                //     // Implement Apple sign-in logic here
-                //     User? user = await authService.signInWithApple();
-                //     if (user != null) {
-                //       print('Apple Login successful: ${user.email}');
-                //     } else {
-                //       print('Apple Login failed');
-                //     }
-                //   },
-                // ),
-              ],
+              onPressed: _login,
+              child: Text('Login'),
             ),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildPhoneNumberInput() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(color: Colors.white),
-      ),
-      child: Row(
-        children: [
-          Text(
-            "+962",
-            style: TextStyle(color: Colors.white, fontSize: 14),
-          ),
-          SizedBox(width: 8),
-          Expanded(
-            child: TextField(
-              controller: phoneNumberController,
-              style: TextStyle(color: Colors.white),
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                hintText: "7X-XXXXXXX",
-                hintStyle: TextStyle(color: Colors.grey),
-                border: InputBorder.none,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _socialButton(String assetName, VoidCallback onTap) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: 60,
-        height: 60,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.white),
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Center(
-          child: SvgPicture.asset(
-            assetName,
-            width: 24,
-            height: 24,
-          ),
         ),
       ),
     );
